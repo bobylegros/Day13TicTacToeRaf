87d5ce65e52fe13e0a23d485391c2f9ef0354699
bobylegros <rafcapdeville@gmail.com>
2018-01-25 14:56:13 +0100

    first

---
+++ board.rb
@@ -0,0 +1,18 @@
+class Board
+attr_accessor :board
+
+  #Initialiser le tableau de jeu 3x3
+  def initialize(board)
+    
+  end
+
+  #Afficher l'état du tableau
+  def afficher_board
+
+  end
+
+  def save_board
+
+  end
+
+end
---
+++ player.rb
@@ -0,0 +1,24 @@
+require_relative 'game'
+require_relative 'board'
+require_relative 'play'
+
+class Player
+
+  def initialize(joueur)
+
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
